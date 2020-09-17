4fd8f2f850f28a50c7517d37d2b7e6e52c0f5dd9
revelatiosgn <revelatiosgn@gmail.com>
2020-09-17 22:53:16 +0300

    portals

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load -1");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            print("Loaded");
+
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this)
+                    continue;
+
+                if (portal.destination != destination)
+                    continue;
+
+                return portal;
+            }
+            
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+    }
+}
